84968102fdbf5841da57fba0d64fb77381b70ebb
Carlos Maria Casado Lopez <carloscasadolopez53@gmail.com>
2026-05-04 16:08:47 +0200

    feat(roles): add user-role assignment functions

--- pkg/roles/roles.go
+++ pkg/roles/roles.go
@@ -3,6 +3,7 @@ package roles
 import (
 	"encoding/json"
 	"fmt"
+	"slices"
 	"time"
 
 	bolt "go.etcd.io/bbolt"
@@ -147,4 +148,95 @@ func (rs *RoleStore) RoleExists(name string) (bool, error) {
 		return nil
 	})
 	return exists, err
+}
+
+// Funcion helper
+
+func getUserRoles(tx *bolt.Tx, username string) (UserRoles, error) {
+	b := tx.Bucket([]byte(bucketUserRoles))
+	if b == nil {
+		return UserRoles{}, fmt.Errorf("bucket de user_roles no encontrado")
+	}
+	data := b.Get([]byte(username))
+	if data == nil {
+		return UserRoles{Username: username, Roles: []string{}}, nil
+	}
+	var ur UserRoles
+	if err := json.Unmarshal(data, &ur); err != nil {
+		return UserRoles{}, err
+	}
+	return ur, nil
+}
+
+// Funciones para administrador
+
+func (rs *RoleStore) AssignRole(username, role string) error {
+	return rs.db.Update(func(tx *bolt.Tx) error {
+		br := tx.Bucket([]byte(bucketRoles))
+		if br == nil {
+			return fmt.Errorf("bucket de roles no encontrado")
+		}
+		if br.Get([]byte(role)) == nil {
+			return fmt.Errorf("el rol '%s' no existe", role)
+		}
+
+		ur, err := getUserRoles(tx, username)
+		if err != nil {
+			return err
+		}
+
+		if slices.Contains(ur.Roles, role) {
+			return fmt.Errorf("el usuario '%s' ya tiene el rol '%s'", username, role)
+		}
+
+		ur.Roles = append(ur.Roles, role)
+
+		data, err := json.Marshal(ur)
+		if err != nil {
+			return err
+		}
+		b := tx.Bucket([]byte(bucketUserRoles))
+		if b == nil {
+			return fmt.Errorf("bucket de user_roles no encontrado")
+		}
+		return b.Put([]byte(username), data)
+	})
+}
+
+func (rs *RoleStore) RemoveRole(username, role string) error {
+	return rs.db.Update(func(tx *bolt.Tx) error {
+		ur, err := getUserRoles(tx, username)
+		if err != nil {
+			return err
+		}
+
+		i := slices.Index(ur.Roles, role)
+		if i == -1 {
+			return fmt.Errorf("el usuario '%s' no tiene el rol '%s'", username, role)
+		}
+		ur.Roles = slices.Delete(ur.Roles, i, i+1)
+
+		data, err := json.Marshal(ur)
+		if err != nil {
+			return err
+		}
+		b := tx.Bucket([]byte(bucketUserRoles))
+		if b == nil {
+			return fmt.Errorf("bucket de user_roles no encontrado")
+		}
+		return b.Put([]byte(username), data)
+	})
+}
+
+func (rs *RoleStore) GetUserRoles(username string) ([]string, error) {
+	var roles []string
+	err := rs.db.View(func(tx *bolt.Tx) error {
+		ur, err := getUserRoles(tx, username)
+		if err != nil {
+			return err
+		}
+		roles = ur.Roles
+		return nil
+	})
+	return roles, err
 }
